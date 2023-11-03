180d695b77250f568f9f0dc3fa8129a489c93344
MeXinuX <angel.curielmnz@uanl.edu>
2023-11-03 00:28:20 -0600

    Eliminando app de django y modificado path por una ruta relativa

--- morgans/settings.py
+++ morgans/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'morgans.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/home/mexinux/Documents/Django-Morgans/morgans/templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
